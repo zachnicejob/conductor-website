1957f21b49dc0efc311ef76232c349669773c0b3
zachnicejob <zachnicel16@gmail.com>
2023-01-17 14:13:34 -0500

    Updated home color scheme

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -44,15 +44,15 @@ const ContactForm = () => {
     
     return (
         <form>
-            <div className="title is-1 is-centered bg-variant-title">Contact Me</div>
+            <div className="title is-1 is-centered">Contact Me</div>
             <div className="field">
-                <label className="label bg-variant-title">Name</label>
+                <label className="label">Name</label>
                 <div className="control">
-                    <input class="input" type="text" placeholder="Name" value={name} onChange={e => setName(e.target.value)} />
+                    <input className="input" type="text" placeholder="Name" value={name} onChange={e => setName(e.target.value)} />
                 </div>
             </div>
             <div className="field">
-                <label className="label bg-variant-title">Email</label>
+                <label className="label">Email</label>
                 <div className="control">
                     {emailSent ? <input className="input" type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} /> : 
                         <input className="input" required type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
@@ -60,14 +60,14 @@ const ContactForm = () => {
                 </div>
             </div>
             <div className="field">
-                <label className="label bg-variant-title">Message</label>
+                <label className="label">Message</label>
                 <div className="control">
                     <textarea className="textarea" placeholder="What's up?" value={message} onChange={e => setMessage(e.target.value)}></textarea>
                 </div>
             </div>
             <div className="field is-grouped">
                 <div className="control">
-                    <button className="button is-link" onClick={submit}>Submit</button>
+                    <button className="button is-primary" onClick={submit}>Submit</button>
                 </div>
                 {/* <div className="control">
                     <button className="button is-link is-light">Cancel</button>
